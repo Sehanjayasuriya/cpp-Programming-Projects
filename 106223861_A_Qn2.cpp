4d47d271b913094229bc4c9d1f02b8eb84f546cd
Sehanjayasuriya <shehanmadusha2006@gmail.com>
2026-06-15 22:41:42 +0530

    Add files via upload

--- 106223861_A_Qn2.cpp
+++ 106223861_A_Qn2.cpp
@@ -1,67 +1,235 @@
-#include <stdlib.h>
-#include <time.h>
-#include "splashkit.h"
+/*
+Name: Sehan Madusha
+Student ID: 106223861
+Name of the C file: 106223861_A_Qn2.cpp
 
-void draw_star(int clr_value, double x, double y, int width, int height){
-    color clr;
-    //declare other variables as in task 3.2
+Asiignment 2 question 2
+Text-based Music Album Application
+This program is a Text-based music album application made using C++.
+It allows the user to manage and play music albums by reading album and track information from an external text file.
+The program uses structures, enumerations, array, file handling, and functions. 
+*/
 
-    switch(clr_value){
-        //write different case to select different colours according to the clr_value
-        case 0: clr = COLOR_RED;
-        break;
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
 
-        case 1: clr = COLOR_GREEN;
-        break;
+//enum for genre
+enum Genre { POP, ROCK, HIPHOP, CLASSICAL };
 
-        case 2: clr = COLOR_BLUE;
-        break;
+//structure for the track
+struct Track {
+    string title;
+    string path;
+};
 
-        case 3: clr = COLOR_YELLOW;
-        break;
+struct Album {
+    string title;
+    string artist;
+    Genre genre;
+    int trackCount;
+    Track tracks[20]; //total number of arrays for  the track
+};
 
-    }    
-    double x2 = x+width;
-    double y2= y+height;
-    double left_x=x-width;
-    double mid_y=y+height/2;
-    double mid_x=x+width/2;
-    double right_x=x2+width;
-    double top_y=y-height;
-    double bottom_y=y2+height;
+//global data
+Album albums[20]; //array that can store 20 albums
+int albumCount = 0; //storing how many albums are currently loaded
 
-    fill_rectangle(clr,x,y,width,height);
+//converting enum values to readable text for display
+string genreToString(Genre g) {
+    if (g == POP) 
+    return "Pop";
 
-    fill_triangle(clr,x,y2,left_x,mid_y,x,y);
-    fill_triangle(clr,x,y,mid_x,top_y,x2,y);
-    fill_triangle(clr,x2,y,right_x,mid_y,x2,y2);
-    fill_triangle(clr,x2,y2,mid_x,bottom_y,x,y2);//here give the commands to create the star
+    if (g == ROCK)
+     return "Rock";
+
+    if (g == HIPHOP)
+     return "HipHop";
+    
+     if (g == CLASSICAL)
+     return "Classical";
+
+    return "Unknown";
 }
 
-int main(){
+//menu option 1
+//read album data from text file
+void readAlbums() {
+    string filename;
+    cout << "Enter a file path to an Album: ";
+    cin >> filename;
 
-    open_window("shape", 800, 600);
-    clear_screen(COLOR_WHITE);
-    srand(time(NULL));
+    ifstream inFile;
+    inFile.open(filename.c_str()); //open file entered by the user
 
-    int row=1, column ;
-    double x = 100, y = 100;
-
-    while(row<=10)
-    {
-        column = 1;
-        while(column <= row){
-            draw_star(rand()%4, x,y,10, 10); //call draw_star function to draw the stars
-            x+=50; //update the x-position here
-            column++;
-        }
-        row++;
-        x=100; //reset the x position so that the next row starts at the same position
-        y+=50; //update the y position here to go to the next row
-
+    //checking file opening
+    if (inFile.fail()) {
+        cout << "File could not be opened.\n";
+        return;
     }
-    refresh_screen(60);
-    delay(50000);
-    return 0;
 
+    //read number of albums
+    inFile >> albumCount;
+    inFile.ignore(); //ignore newline
+
+    //read each album
+    for (int i = 0; i < albumCount; i++) {
+        getline(inFile, albums[i].artist);
+        getline(inFile, albums[i].title);
+
+        int g;
+        inFile >> g; //reading genre number
+        albums[i].genre = static_cast<Genre>(g);  //conerting it to enum type
+
+        inFile >> albums[i].trackCount;//read number of tracks
+        inFile.ignore();
+
+        //reading every track title and file path in text file
+        for (int j = 0; j < albums[i].trackCount; j++) {
+            getline(inFile, albums[i].tracks[j].title);
+            getline(inFile, albums[i].tracks[j].path);
+        }
+    }
+
+    inFile.close();
+    cout << "Albums loaded successfully.\n";
+}
+
+//main menu option 2(display album)
+void displayAlbumMenu() {
+    int choice;
+    do {
+        cout << "\nDisplay Album Menu\n";
+        cout << "1. Display all album\n";
+        cout << "2. Display album by genre\n";
+        cout << "3. Back to main menu\n";
+        cout << "Please enter your choice: ";
+        cin >> choice;
+
+        //if user enter 1 displaying album title, artist, genre
+        if (choice == 1) {
+            for (int i = 0; i < albumCount; i++) {
+                cout << i + 1 << ". Title : " <<albums[i].title << ", Artist : "
+                     << albums[i].artist << ", genre : "
+                     << genreToString(albums[i].genre) << endl;
+            }
+        }
+        //if user enter 2 displaying genre and asking the the user to select genre
+        else if (choice == 2) {
+            int g;
+            cout << "0. Pop  1. Rock  2. HipHop  3. Classical\n";
+            cout << "Pick a genre\n";
+            cin >> g;
+
+            for (int i = 0; i < albumCount; i++) {
+                if (albums[i].genre == g) {
+                    cout << "Title: "<< albums[i].title << "; Artist Name: "
+                         << albums[i].artist << "; Genre: "
+                         << genreToString(albums[i].genre) << endl;
+                }
+            }
+        }
+
+    } while (choice != 3);
+}
+
+//option 3 (selecting an album to play)
+void playAlbum() {
+
+    //showing albums
+    for (int i = 0; i < albumCount; i++) {
+        cout << i + 1 << ". Title: "
+             << albums[i].title << "; Artist Name: "
+             << albums[i].artist << "; Album Genre: "
+             << genreToString(albums[i].genre) << endl;
+    }
+
+    int a;
+    cout << "Select an album to play: ";
+    cin >> a;
+    a--;
+
+    //display tracks
+    for (int i = 0; i < albums[a].trackCount; i++) {
+        cout << i + 1 << ". Track name: " << albums[a].tracks[i].title << endl;
+    }
+
+    int t;
+    cout << "Select a track to play: ";
+    cin >> t;
+    t--;
+
+    cout << "Playing track of "
+         << albums[a].artist
+         << " from album "
+         << albums[a].title << endl;
+
+    //opening the music file using windows default media player
+    system(("start " + albums[a].tracks[t].path).c_str());
+}
+
+//option 4 (update an existing album)
+void updateAlbum() {
+
+    //show all the albums
+    for (int i = 0; i < albumCount; i++) {
+        cout << i + 1 << ". Title: "
+             << albums[i].title << "; Artist: "
+             << albums[i].artist << "; Genre: "
+             << genreToString(albums[i].genre) << endl;
+    }
+
+    int a;
+    cout << "Enter the album to edit: ";
+    cin >> a;
+    a--;
+
+    //letting the user choose what to edit title or genre
+    int edit;
+    cout << "1. Title\n2. Genre\n";
+    cout << "Enter the selection: ";
+    cin >> edit;
+    cin.ignore();
+
+    //editing album title
+    if (edit == 1) {
+        cout << "Title: ";
+        getline(cin, albums[a].title);
+    }
+
+    //editing album genre
+    else if (edit == 2) {
+        int g;
+        cout << "0.Pop 1.Rock 2.HipHop 3.Classical\n";
+        cout << "Enter number of genre: ";
+        cin >> g;
+        albums[a].genre = static_cast<Genre>(g); // update genre using enum value
+    }
+}
+
+int main() {
+    int choice;
+
+    //repeats until user choose exit
+    do {
+        cout << "\nMain Menu\n";
+        cout << "1. Read in Albums\n";
+        cout << "2. Display Album\n";
+        cout << "3. Select an Album to play\n";
+        cout << "4. Update an existing album\n";
+        cout << "5. Exit the application\n";
+        cout << "Please enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1: readAlbums(); break;
+        case 2: displayAlbumMenu(); break;
+        case 3: playAlbum(); break;
+        case 4: updateAlbum(); break;
+        }
+    } while (choice != 5);
+
+    return 0;
 }
--- 106223861_A_Qn3.cpp
+++ 106223861_A_Qn3.cpp
@@ -1,64 +1,152 @@
+/*
+Name : Sehan Madusha
+student ID : 106223861
+Name of the C file: 106223861_A_Qn3.cpp
+
+Assignment 2 question 3
+This program generates an array of 800 bars with random colors and heights 
+drawing them as vertical bars, and allows the user to sort them using either bubble sort(sort 1) or selection sort(sort 2)
+user can sort by clicking on sort buttons
+*/
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include "splashkit.h"
+#define SIZE 800
 using namespace std;
 
-//function prototype
-int roll_dice();
-void play_game();
+//struct
+struct sample{
+    int value;
+    color clr;
+};
 
-int main(){
-    srand(time(NULL)); // to get a different result each time
-    play_game(); //calling the function to play
-    return 0;   
+color get_color(int num){
+    float hue;
+    hue = static_cast<float>(num)/screen_height();
+    return hsb_color(hue,0.7,0.8);
+
 }
 
-int roll_dice(){
-    return rand() % 6+1; //generates a random number between 1 and 6
-}
-
-//function declaration
-void play_game(){
-    int dice_1, dice_2, dice_3, sum, point;
-
-    // first roll
-    dice_1=roll_dice();
-    dice_2=roll_dice();
-    dice_3=roll_dice();
-
-    //gerring the sum of these dice values
-    sum= dice_1+ dice_2 + dice_3;
-    cout << "player rolled: " << dice_1 << " + " << dice_2 << " + " << dice_3 << " = " << sum << endl;
-
-    if(sum >= 13){
-        cout << "Player wins" << endl;
-    }
-    else if(sum <=6){
-        cout << "Player loses" << endl;
-    }
-    else{
-        //if the sum is between 7 and 12 sum = points
-        point = sum;
-        cout << "Point is " << point << endl;
-
-        //continue rolling until sum >=15
-        while(true){
-            dice_1=roll_dice();
-            dice_2=roll_dice();
-            dice_3=roll_dice();
-
-            sum = dice_1 + dice_2 + dice_3;
-            cout << "player rolled: " << dice_1 << " + " << dice_2 << " + " << dice_3 << " = " << sum << endl;
-
-            if(sum==point){
-                cout << "Player wins " << point << endl;
-                break;
+//sort 1 method bubble sort
+void BubbleSort(sample data[]){
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        for (int j = 0; j < SIZE - i - 1; j++)
+        {
+            if (data[j].value > data[j + 1].value)
+            {
+                int temp = data[j].value;
+                data[j].value = data[j + 1].value;
+                data[j + 1].value = temp;
             }
-            else if(sum >= 15){
-                cout << "Player loses" << endl;
-                break;
-            }
-
         }
     }
+
+}
+
+//sort 2 selection sort
+void SelectionSort(sample data[]){
+    for(int i = 0; i < SIZE - 1; i++){
+        int min_pos = i;
+
+        //finding largest value
+        for(int j = i + 1; j < SIZE; j++)
+            if(data[j].value < data[min_pos].value){
+                min_pos = j;
+            }
+
+        //swapping the value
+        if(min_pos != i){
+            int temp = data[i].value;
+            data[i].value = data[min_pos].value;
+            data[min_pos].value = temp;
+        }
+    }
+
+}
+
+
+void fill_array(sample data[]){
+    int i;
+    for(i=0; i < SIZE; i++ ){
+        data[i].value = rand() % screen_height()-100;
+        data[i].clr = get_color(data[i].value);
+    }
+
+}
+
+void draw_bar(sample data[]){
+    int i;
+    float x, y;
+    float rect_width = 1;
+    for(i=0 ; i < SIZE; i++){
+        x = rect_width * i ;
+        y = screen_height() - data[i].value;
+        fill_rectangle(data[i].clr, x, y, rect_width, data[i].value);
+    }
+
+}
+
+//detecting button clicks
+bool clicked_button(float x, float y, float w, float h){
+    return mouse_clicked(LEFT_BUTTON) &&
+        mouse_x() >= x && mouse_x() <= x + w &&
+        mouse_y() >= y && mouse_y() <= y + h;
+}
+
+//drawing three buttons and red separation line
+void DrawButton(){
+
+    //draw button
+    fill_rectangle(COLOR_GREEN, 10, 10, 120, 30);
+    draw_text("Draw Bars", COLOR_BLACK, 25, 17);
+
+    //sort 1 button
+    fill_rectangle(COLOR_GREEN, 140, 10, 120, 30);
+    draw_text("Sort 1", COLOR_BLACK, 175, 17);
+
+    //sort 2 button
+    fill_rectangle(COLOR_GREEN, 270, 10, 120, 30);
+    draw_text("Sort 2", COLOR_BLACK, 305, 17);
+
+    //red line
+    fill_rectangle(COLOR_RED, 0, 87, 800, 5);
+}
+
+int main(){
+    sample data[SIZE];
+    open_window("Array rectangles", 800, 700);
+    srand(time(NULL));
+    fill_array(data);
+
+    while(not quit_requested()){
+
+        process_events();
+
+        //if draw button is clicked, draw random bar selection
+        if(clicked_button(10, 10, 120, 30)){
+            clear_screen(COLOR_WHITE);
+            fill_array(data);
+        }
+
+        //if sort 1 button is clicked, draws bubblt sort bar
+        if(clicked_button(140, 10, 120, 30)){
+            fill_array(data);
+            clear_screen(COLOR_WHITE);
+            BubbleSort(data);
+        }
+
+        //if sort 2 button is clicked, draw selection sort bar
+        if(clicked_button(270, 10, 120, 30)){
+            fill_array(data);
+            clear_screen(COLOR_WHITE);
+            SelectionSort(data);
+        }
+        DrawButton();
+        draw_bar(data);
+        refresh_screen(60);
+    }
+    return 0;
 }
